29bfe55fb7911f59bc37a5c0140ebeb7671f5524
AEnyChris <enyonejoseph@gmail.com>
2023-09-15 14:23:13 +0100

    terraform files for the airflow deployment added

---
+++ deploy.ps1
@@ -0,0 +1,32 @@
+echo "### getting cluster credentials to initiate kubectl"
+gcloud container clusters get-credentials $(terraform output -raw kubernetes_cluster_name) --region $(terraform output -raw location)
+echo "### done"
+
+echo "### creating namespace nfs and the nfs-server.yaml file apply"
+kubectl create namespace nfs # creates namespace
+kubectl -n nfs apply -f nfs/nfs-server.yaml # creates server
+$env:NFS_SERVER=$(kubectl -n nfs get service/nfs-server -o jsonpath="{.spec.clusterIP}")
+echo "--done"
+
+echo "-### reating namespace storage"
+kubectl create namespace storage
+
+echo "### using helm to add nfs-subdir-external-provisioner repo"
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+echo "### done"
+
+echo "### intalling nfs-subdir-external-provisioner"
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --namespace storage --set nfs.server=$($env:NFS_SERVER) --set nfs.path=/
+
+echo "### creating namespace airflow"
+kubectl create namespace airflow
+
+echo "### installing airflow commence"
+helm repo add apache-airflow https://airflow.apache.org
+
+helm upgrade --install airflow -f airflow_values.yaml apache-airflow/airflow --namespace airflow
+echo "### done"
+kubectl get pods -n airflow
+
+echo "####port forwarding for airflow webserver##"
+kubectl port-forward svc/airflow-webserver 8080:8080 --namespace airflow
---
+++ terraform.tfvars
@@ -0,0 +1,16 @@
+project_id = "sodium-mountain-396818"
+region     = "us-east1"
+location     = "us-east1-b"
+
+#GKE
+gke_num_nodes = 2
+machine_type  = "n1-standard-1"
+
+#CloudSQL
+instance_name     = "data-bootcamp-1"
+database_version  = "POSTGRES_12"
+instance_tier     = "db-f1-micro"
+disk_space        = 10
+database_name     = "dbname"
+db_username       = "dbuser"
+db_password       = "dbpassword"
