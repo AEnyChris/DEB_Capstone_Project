50051c90404479b3aef7239eca3f1c0644d17d54
AEnyChris <enyonejoseph@gmail.com>
2023-10-07 01:24:48 +0100

    restored dataproc example, test successful

--- dags/dataproc_example.py
+++ dags/dataproc_example.py
@@ -3,9 +3,9 @@ from datetime import timedelta
 from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.dataproc import  DataprocCreateClusterOperator
 from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
-# from airflow.providers.google.cloud.operators.dataproc import DataprocDeleteClusterOperator
-# from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-# from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocDeleteClusterOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 
 default_args = {
     'depends_on_past': False   
